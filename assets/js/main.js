6dad2da5c1ac37de203b90735d7dd303800c5791
axprogramer <105089786+axprogramer@users.noreply.github.com>
2022-05-31 09:30:27 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+function myQuiz1(){
+    document.getElementById("myText").innerText = "Body Language with the words:\n\nHeadache\ncold\nfever\ntoothache\ncough";
+    document.getElementById("exampleModalLabel").innerText = "Number 01";
+}
+function myQuiz2(){
+    document.getElementById("myText").innerText = "Spell 5 words from page 66-67";
+    document.getElementById("exampleModalLabel").innerText = "Number 02";
+}
+function myQuiz3(){
+    document.getElementById("myText").innerText = "គូសមុខសេរី 🤣";
+    document.getElementById("exampleModalLabel").innerText = "Number 03";
+}
+function myQuiz4(){
+    document.getElementById("myText").innerText = "Let other Ss call 3 words to spell";
+    document.getElementById("exampleModalLabel").innerText = "Number 04";
+}
+function myQuiz5(){
+    document.getElementById("myText").innerText = "Reset score 😭";
+    document.getElementById("exampleModalLabel").innerText = "Number 05";
+}
+function myQuiz6(){
+    document.getElementById("myText").innerText = "+5 😊";
+    document.getElementById("exampleModalLabel").innerText = "Number 06";
+}
+function myQuiz7(){
+    document.getElementById("myText").innerText = "What is the title of unit 8";
+    document.getElementById("exampleModalLabel").innerText = "Number 07";
+}
+function myQuiz8(){
+    document.getElementById("myText").innerText = "-2 🤗";
+    document.getElementById("exampleModalLabel").innerText = "Number 08";
+}
+function myQuiz9(){
+    document.getElementById("myText").innerText = "Again 😎";
+    document.getElementById("exampleModalLabel").innerText = "Number 09";
+}
+function myQuiz10(){
+    document.getElementById("myText").innerText = "FuckYOU! 🦾🐱‍🏍";
+    document.getElementById("exampleModalLabel").innerText = "Number 10";
+}
+function myQuiz11(){
+    document.getElementById("myText").innerText = "Get number 6 (If it gets already, you will get nothing!";
+    document.getElementById("exampleModalLabel").innerText = "Number 11";
+}
+function myQuiz12(){
+    document.getElementById("myText").innerText = "You got fire 😜 ";
+    document.getElementById("exampleModalLabel").innerText = "Number 12";
+}
